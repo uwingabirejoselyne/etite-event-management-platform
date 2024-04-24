afe8ea228f6915a1cbd4be448c23bb205ca9c47a
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-24 23:01:08 +0200

    event creation

--- controller/eventController.js
+++ controller/eventController.js
@@ -1,14 +1,14 @@
-const Event = require('../models/eventModel')
+const Event = require('../models/eventModel');
 
-const createEvent = async(req,res)=>{
+const createEvent = async (req, res) => {
+    console.log(req.body);
     try {
-        const newEvent = await Event.create(req.body)
-        res.json(newEvent)
-        
+        const newEvent = await Event.create(req.body);
+        res.json(newEvent);
     } catch (error) {
         console.log(error);
-        throw new Error(error)
+        res.status(500).json({ error: 'Internal Server Error' });
     }
+};
 
-}
-module.exports= createEvent
+module.exports = {createEvent};
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 dbConnect()
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const eventRoute = require('./routes/eventRoute')
 
+app.use('/api/event',eventRoute)
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
