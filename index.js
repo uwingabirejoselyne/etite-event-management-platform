24f6691a0347f15fc730984734377fca3c253b84
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-24 13:25:31 +0200

    nodemon installation

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
+const app = express();
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+dbConnect()
+
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
