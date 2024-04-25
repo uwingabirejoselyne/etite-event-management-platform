b22663aefd572779894b45cb950f3c9294076f79
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-25 07:29:17 +0200

    creation of user model

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose'); 
+
+
+var userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+      },
+      lastname: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      mobile: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      bookings:[{
+        eventId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event'
+        },
+        numberOfTickets: {
+             type: Number,
+      required: true
+        }
+      }]
+    
+});
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
