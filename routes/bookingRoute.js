0873242b2c089f0116782fd0059d0c7292b25dd5
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-25 18:42:02 +0200

    creation of updating bookings functionality

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {createBooking,getUserBookings } = require('../controller/bookingController')
+const {createBooking,getUserBookings,updatingBookings } = require('../controller/bookingController')
 router.post('/',createBooking)
 router.post('/',createBooking)
 router.get('/:eventId',getUserBookings)
+router.put('/:bookingId',updatingBookings)
+
 
 
 module.exports = router
