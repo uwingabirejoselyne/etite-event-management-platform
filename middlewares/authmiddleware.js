01f2564f962f3ebd9a836d80f632cb2cd80bd344
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-25 19:25:16 +0200

    creation of jsonWebToken

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,1 +1,37 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const authMiddleware = async (req, res, next) => {
+  let token;
+  if (req?.headers?.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error("Not Authorized token expired, please login again");
+    }
+  } else {
+    throw new Error("Authorization header missing or invalid");
+  }
+}
+
+const isAdmin = async (req, res, next) => {
+  try {
+  const  {email}  = req.body;
+    const adminUser = await User.findOne({ email });
+    if (!adminUser || adminUser.role !== "admin") {
+      throw new Error('You are not an admin');
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { authMiddleware, isAdmin };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose'); 
+const bcrypt = require("bcrypt");
+const crypto = require('crypto');
 
 
 var userSchema = new mongoose.Schema({
@@ -24,6 +26,10 @@ var userSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
+      role: {
+        type: String,
+        default: "admin",
+      },
       bookings:[{
         eventId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -36,6 +42,22 @@ var userSchema = new mongoose.Schema({
       }]
     
 });
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    next()
+  }
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.createPasswordResetToken = async function (){
+  const resetToken = crypto.randomBytes(32).toString("hex")
+  this.passwordResetToken = crypto.createHash('sha256').update('resettoken').digest("hex")
+  this.passwordChangeAt = Date.now() +30*60*1000;
+  return resetToken
+}
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
