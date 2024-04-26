4f1c67e027cb4a86ccd52c752ab95cae54d51cd7
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-26 13:02:45 +0300

    navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import MenuIcon from '../assets/MenuIcon'
-import Logo from '../assets/Logo'
+import Logo from '../assets/logo.jpg'
 import ProfileIcon from '../assets/ProfileIcon'
 const Navigation = () => (
   <div className=" flex items-center justify-between rounded-b-lg border border-indigo-tertiary bg-indigo-tertiary px-5 text-white">
@@ -8,7 +8,7 @@ const Navigation = () => (
       <MenuIcon />
     </Link>
     <Link to="/user">
-      <Logo className="relative top-3 w-16" />
+      <img src={Logo} className="relative top-3 w-16" alt="" />
     </Link>
     <Link to="/landingpage">
       <ProfileIcon className="h-6 w-6" />
