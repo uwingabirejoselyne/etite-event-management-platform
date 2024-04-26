9d15b228b03625b044b4d3e6fcc593fdb66db81f
Joselyne uwingabire <uwingajoselyne@gmail.com>
2024-04-26 13:08:52 +0300

    landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -6,12 +6,12 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col gap-10 px-4">
       <p className="flex flex-col items-center pt-24 font-lora text-5xl text-white">
-        <span className="font-bold uppercase">moni</span>
-        <span className="font-medium italic">share</span>
+        <span className="font-bold uppercase">Etite</span>
+        <span className="font-medium italic">Event Management</span>
       </p>
       <p className="flex flex-col items-center truncate py-10 font-lora text-xl font-thin text-white sm:px-14 sm:text-2xl">
-        <span>Start Sharing Your Monis</span>
-        <span>wiht the world</span>
+        <span>Start to see our event</span>
+        <span>using our website</span>
       </p>
       <Button color="filled" className="mx-auto w-full max-w-md" onClick={() => navigate('/login')}>
         Log in
